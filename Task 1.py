3a047ad627aa5a6bcb097ed512cb059dec7d982d
drdeepakbhardwaj13-pixel <drdeepakbhardwaj13@gmail.com>
2025-11-06 15:24:58 +0530

    Initial commit

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+A = input("Enter the first number:")
+B = input("Enter the second number:")
+A = int(A)
+B = int(B)
+
+Addition = A + B
+Subtraction = A - B
+Multiplication = A * B
+Division = A / B
+
+print('Addition:' , Addition)
+print('Subtraction:' , Subtraction)
+print('Multiplication:' , Multiplication)
+print('Division:' , Division)
